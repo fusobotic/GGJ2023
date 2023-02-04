9404a340a3ceb41f08f717cfccde03360e1ee989
Christian Coppoletti <fusionstudio3d@gmail.com>
2023-02-04 16:40:01 -0500

    More UI touchups

--- SOURCE/Assets/Scripts/DayCounter.cs
+++ SOURCE/Assets/Scripts/DayCounter.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DayCounter : MonoBehaviour
 {
@@ -9,7 +10,9 @@ public class DayCounter : MonoBehaviour
     private bool changingDay = false;
 
     [SerializeField]
-    private DayNightControl dnc;
+    private DayNightControl dayNightControl;
+    [SerializeField]
+    private Toggle[] calendarDays;
     
     //use this bool to check if we're in the middle of a transition or not
     public bool ChangingDay {
@@ -18,15 +21,16 @@ public class DayCounter : MonoBehaviour
 
     public IEnumerator AdvanceDay()
     {
-        if (!changingDay && dayIndex <= dayMax)
+        if (!changingDay && dayIndex < dayMax)
         {
             changingDay = true;
+            calendarDays[dayIndex].isOn = true;
             dayIndex++;
-            dnc.ChangeToNight();
-            yield return new WaitForSeconds(dnc.CycleTime()*2);
+            dayNightControl.ChangeToNight();
+            yield return new WaitForSeconds(dayNightControl.CycleTime()*2);
             changingDay = false;
         }
-        else if (!changingDay && dayIndex > dayMax)
+        else if (!changingDay && dayIndex == dayMax)
         {
             //lose condition?
             //trigger garbage truck animation and then lose screen
